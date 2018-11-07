2644c976ac389c0a957dc3dfe8b7b82ff03b7b4b
Girish B <tuxinvincible@gmail.com>
2018-11-07 21:53:33 +0530

    String Splitter examples

---
+++ src/com/girish/simple/string/StringSplitter.java
@@ -0,0 +1,14 @@
+package com.girish.simple.string;
+
+import java.util.Arrays;
+
+public class StringSplitter {
+
+	public static void main(String[] args) {
+		String str = "banks|123456";
+		System.out.println(str.split("\\|").length);
+		Arrays.stream(str.split("\\|")).forEach(System.out::println);
+
+	}
+
+}
