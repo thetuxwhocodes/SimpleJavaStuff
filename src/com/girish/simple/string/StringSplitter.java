5191bb8b0eb9974c9922db341c27b82a221b5d39
Girish B <tuxinvincible@gmail.com>
2018-11-08 22:27:59 +0530

    BitBucket test

--- src/com/girish/simple/string/StringSplitter.java
+++ src/com/girish/simple/string/StringSplitter.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class StringSplitter {
 
 	public static void main(String[] args) {
-		String str = "banks|123456";
+		String str = "banks|123456|11";
 		System.out.println(str.split("\\|").length);
 		Arrays.stream(str.split("\\|")).forEach(System.out::println);
 
