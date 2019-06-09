816312c9ca1e70197f5b13aee5505c38728dc038
Girish B <tuxinvincible@gmail.com>
2019-06-09 15:02:53 +0530

    Recursion Stuff - String reversal using recursion

---
+++ src/com/girish/simple/recursive/StringRecursion.java
@@ -0,0 +1,20 @@
+package com.girish.simple.recursive;
+
+public class StringRecursion {
+
+	public static void main(String[] args) {
+		String str = "123456789";
+		System.out.println(reverse(str));
+	}
+
+	private static String reverse(String str) {
+		
+		if(str == null || str.length() <= 1) {
+			return str;
+		}
+		
+		return reverse(str.substring(str.length() -1, str.length() ) )  + reverse(str.substring(0, str.length() -1));
+		
+	}
+
+}
